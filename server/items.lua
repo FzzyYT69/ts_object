9db58ff93edc63afa08a0010a83576d8773f88a4
FzzY <99145322+FzzyYT69@users.noreply.github.com>
2024-08-07 14:36:13 +0500

    Update items.lua

--- server/items.lua
+++ server/items.lua
@@ -2,6 +2,6 @@
 --[[
 exports.qbx_core:CreateUseableItem('weed_brick', function(source, item)
     local src = source
-    TriggerClientEvent('uus_object:client:placeObject', src, 'prop_mb_cargo_03a', 'weed_brick')
+    TriggerClientEvent('uus_object:client:placeObject', src, 'prop_mb_cargo_03a', 'weed_brick', item.info)
 end)
 ]]
